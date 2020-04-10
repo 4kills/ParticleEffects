b0458b1a7d53b9fa962ad60528b9f6b44cdac159
4kills <dominik.ochs@gmx.net>
2020-04-10 22:02:08 +0200

    removed redundant methods and added doc strings

--- src/main/java/net/_4kills/particles/math/MatrixConstructor.java
+++ src/main/java/net/_4kills/particles/math/MatrixConstructor.java
@@ -1,11 +1,16 @@
 package net._4kills.particles.math;
 
-import org.ejml.data.DMatrix3;
 import org.ejml.data.DMatrix3x3;
 
-final class MatrixConstructor {
-    private MatrixConstructor() {} //defacto static class
-
+/**
+ * Constructs commonly used matrices
+ */
+abstract class MatrixConstructor {
+    /**
+     * Returns the identity matrix in R^3. That is the matrix that has 1 on its diagonal and 0 everywhere else.
+     *
+     * @return the identity matrix
+     */
     static DMatrix3x3 identity3x3Matrix() {
         final DMatrix3x3 m = new DMatrix3x3();
         m.a11 = 1;
@@ -14,7 +19,13 @@ final class MatrixConstructor {
         return m;
     }
 
-    static DMatrix3x3 Ry (final double theta) {
+    /**
+     * Returns the matrix that rotates about the y-axis by theta (in radians)
+     *
+     * @param theta the angle of rotation in radians
+     * @return the rotation matrix
+     */
+    static DMatrix3x3 Ry(final double theta) {
         final DMatrix3x3 m = new DMatrix3x3();
         m.a11 = Math.cos(theta);
         m.a13 = Math.sin(theta);
@@ -24,44 +35,19 @@ final class MatrixConstructor {
         return m;
     }
 
-    static DMatrix3x3 Rz (final double theta){
+    /**
+     * Returns the matrix that rotates about the z-axis by theta (in radians)
+     *
+     * @param theta the angle of rotation in radians
+     * @return the rotation matrix
+     */
+    static DMatrix3x3 Rz(final double theta) {
         final DMatrix3x3 m = new DMatrix3x3();
         m.a11 = Math.cos(theta);
         m.a12 = -Math.sin(theta);
         m.a21 = Math.sin(theta);
         m.a22 = Math.cos(theta);
         m.a33 = 1;
-        return m;
-    }
-
-    static DMatrix3x3 toXZPlane (final DMatrix3 vec) {
-        if(vec.a1 == 0 && vec.a2 == 0) {
-            return identity3x3Matrix();
-        }
-
-        final DMatrix3x3 m = new DMatrix3x3();
-        final double z = Math.sqrt(vec.a1 * vec.a1 + vec.a2 * vec.a2);
-        m.a11 = vec.a1 / z;
-        m.a12 = vec.a2 / z;
-        m.a21 = -vec.a2 / z;
-        m.a22 = vec.a1 / z;
-        m.a33 = 1;
-        return m;
-    }
-
-    static DMatrix3x3 toZAxis(final DMatrix3 vec) {
-        if(vec.a1 == 0 && vec.a2 == 0) {
-            return identity3x3Matrix();
-        }
-
-        final DMatrix3x3 m = new DMatrix3x3();
-        final double z = Math.sqrt(vec.a1 * vec.a1 + vec.a2 * vec.a2);
-        final double u = Ops.calcLength(vec);
-        m.a11 = vec.a3 / u;
-        m.a13 = -z / u;
-        m.a22 = 1;
-        m.a31 = z / u;
-        m.a33 = vec.a3 / u;
         return m;
     }
 }
